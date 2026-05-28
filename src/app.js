8f54771a448e9ca4dc84203099c676eaf3fd411d
Iztel <Iztel@ITZELL>
2026-05-28 13:46:36 -0600

    Actualizando app.js con ruta raíz

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
+// Esta es la ruta raíz que pediste para que no dé error 404
+app.get("/", (req, res) => {
+  res.send("API de Productos funcionando correctamente");
+});
+
+// Tus rutas de productos
 app.use("/api/products", productsRoutes);
 
 export default app;
